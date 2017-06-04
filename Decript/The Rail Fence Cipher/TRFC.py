70c8191bf0c2439926e4ff78deea28dcc89e4c79
Rootkit <1576235794@qq.com>
2017-06-04 22:37:08 +0800

    Add files via upload

---
+++ Decript/The Rail Fence Cipher/TRFC.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+def decrypt(Str, x, Len):
+	tmp = ''
+	if(Len%x == 0):
+		L1 = int(Len/x)
+		for z in range(x):
+			for y in range(0,L1):
+				tmp += Str[x * y + z]
+				#print(x,y,tmp)
+	return tmp
+
+print('Please input what you wanna decrypt:')
+Input = input()
+Len = len(Input)
+result = ''
+print('**************\n\nResults is :')
+for x in range(2,Len):		#因子
+	if(Len%x == 0):
+		result = decrypt(Input, x, Len)
+	#test	print('[+] key=%s   result= %s'%(x,result))
+print('\n**************')
