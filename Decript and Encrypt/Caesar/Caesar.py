a164ca8f46fd992360ba53f91bde32d56053ddd4
rootkit <1576235794@qq.com>
2018-12-14 19:36:09 +0800

    update

--- Decript and Encrypt/Caesar/Caesar.py
+++ Decript and Encrypt/Caesar/Caesar.py
@@ -1,24 +1,24 @@
 #! /usr/bin/python3
 
+from string import ascii_lowercase as lower
+from string import ascii_uppercase as upper
+
 import argparse 
+
 pars = argparse.ArgumentParser()
 pars.add_argument('-k',help='移位数')
 pars.add_argument('-s',help='待加解密字符串')
 parsers = pars.parse_args()
-L_letter = 'abcdefghijklmnopqrstuvwxyz'
-U_letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-result = ''
-Input = ''
+
 
 def decrypt(k, s):
-	global result
+	result = ''
 	for strLen in range(len(s)):
 		c = s[strLen]
-			#result += c.islower()?L_letter[key+Input.index(c)]:(c.isupper()?U_letter[key+Input.index[c]]:c)
-			#好吧，py里面没有三目运算符。。。
-		result += L_letter[(int(k)+L_letter.index(c))%26] if c.islower() else (U_letter[(int(k)+U_letter.index(c))%26] if c.isupper() else c)
+		#result += c.islower()?lower[key+Input.index(c)]:(c.isupper()?upper[key+Input.index[c]]:c)
+	    	#好吧，py里面没有三目运算符。。。
+		result += lower[(int(k)+lower.index(c))%26] if c.islower() else (upper[(int(k)+upper.index(c))%26] if c.isupper() else c)
 	print('[+]  key= %s  result= %s'%(k,result))
-	result = ''
 	return result
 
 if (parsers.k == None):
